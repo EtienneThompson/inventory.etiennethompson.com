a53e6c46f54c9f89c911824e75a0848ad65c9f8d
Etienne Thompson <et@etiennethompson.com>
2021-12-31 11:56:59 -0700

    Update Toolbar with Grid components.

--- src/components/common/Toolbar/Toolbar.tsx
+++ src/components/common/Toolbar/Toolbar.tsx
@@ -1,11 +1,18 @@
 import { Button } from "../Button";
+import { Row, Col } from "../Grid";
 import "./Toolbar.scss";
 
 export const Toolbar = () => {
   return (
     <div className="toolbar-container">
-      <div className="title">Etienne Thompson Inventory</div>
-      <Button>Login</Button>
+      <Row>
+        <Col cols={2} align="start">
+          <div className="title">Etienne Thompson Inventory</div>
+        </Col>
+        <Col cols={2} align="end">
+          <Button>Login</Button>
+        </Col>
+      </Row>
     </div>
   );
 };
