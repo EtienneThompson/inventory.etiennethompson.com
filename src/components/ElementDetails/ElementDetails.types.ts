5a4a1bd985bda19a75510a96130d5dde9e67f258
Etienne Thompson <et@etiennethompson.com>
2022-01-09 21:23:49 -0700

    Implement deleting functionality.

--- src/components/ElementDetails/ElementDetails.types.ts
+++ src/components/ElementDetails/ElementDetails.types.ts
@@ -1,11 +1,21 @@
 export interface ElementDetailsProps {
   element: ElementComponents;
+  type: string;
+  numChildren?: number;
 }
 
 export interface ElementComponents {
+  folderid?: string;
+  itemid?: string;
   name: string;
   picture: string;
   description: string;
+  parent_folder: string;
   created: string;
   updated: string;
+}
+
+export interface DeleteRequest {
+  folderid?: string;
+  itemid?: string;
 }
