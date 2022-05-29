d5e2ea285f0799a6ebd4d35f7c7c24f9ecc2f9f4
Etienne Thompson <43585499+EtienneThompson@users.noreply.github.com>
2022-05-29 04:24:45 -0700

    Add description to landing page. (#23)

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -53,6 +53,25 @@ export const LandingPage = () => {
   return (
     <Container className="landing-page-container">
       <h2>Welcome to inventory.etiennethompson.com!</h2>
+      <p className="landing-page-description">
+        This site is a web-based inventory application to help people stay
+        organized with their things. It works similarly to a file system.
+        "Folders" representing real-life objects which contain other objects
+        (such as a box, a bag, or a room). "Items" represent real-life objects
+        which are things you use (such as your phone, a lamp, or a book). The
+        way you organize your items in the inventory system is up to you, but
+        the idea is to help you find your things faster.
+      </p>
+      <div className="horizontal-break" />
+      <p className="landing-page-description">
+        You can check out more about me here:{" "}
+        <a href="http://etiennethompson.com">etiennethompson.com</a>
+        <br />
+        You can check out the code for this project here:{" "}
+        <a href="https://github.com/EtienneThompson/inventory.etiennethompson.com">
+          Github
+        </a>
+      </p>
       {isLoading && <LoadingSpinner />}
       {!isLoading && folder && (
         <Row className="base-folder-card" onClick={onBaseFolderClicked}>
